9092199f31b5f9d3a128e592c51d25ee734b7746
Stoyan610 <stoyan.k-n@yandex.ru>
2014-05-25 21:44:42 +0400

    sovsem_ne_final

--- mainadmin/config_address.php
+++ mainadmin/config_address.php
@@ -9,12 +9,12 @@ $sample = '~[^\\/]*[\\/][^\\/]*$~';
 $pure_root = preg_replace($sample, '', $root);
 define('PATH', $pure_root);
 
-//URL-адрес админки - Для Денвера 'mainadmin.toyshop.local/' , а для MAMP 'http://localhost:8888/toyshop.local/mainadmin/'
-define('ADMINURL', 'http://localhost:8888/toyshop.local/mainadmin/');
+//URL-адрес админки - Для Денвера 'mainadmin.toyshop.local/' , а для MAMP 'http://localhost:80/toyshop.local/mainadmin/'
+define('ADMINURL', 'http://localhost:80/toyshop.local/mainadmin/');
 //define('ADMINURL', 'http://mainadmin.toyshop.local/');
 
 //URL-адрес сайта - Для Денвера 'mainadmin.toyshop.local/' , а для MAMP 'http://localhost:8888/toyshop.local/www/'
-define('SITEURL', 'http://localhost:8888/toyshop.local/www/');
+define('SITEURL', 'http://localhost:80/toyshop.local/www/');
 //define('SITEURL', 'http://toyshop.local/');
 
 //Для Денвера пароль '', а для MAMP пароль 'root'
--- www/views/pages/templates/ordertoy.tpl
+++ www/views/pages/templates/ordertoy.tpl
@@ -1,5 +1,5 @@
 <tr id='%toyid%'>
-  <td class='td2'><a href='toyitem.php'><img height='70px' width='70px' src='%site_url%pictures/toy70x70/%toyfilename%.jpg' alt='%toyname%' /></a></td>
+  <td class='td2'><a href='index.php?page=toyitem&toy=%toyid%'><img height='70px' width='70px' src='%site_url%pictures/toy70x70/%toyfilename%.jpg' alt='%toyname%' /></a></td>
   <td class='td1'><a href='index.php?page=toyitem&toy=%toyid%'>%toyname%</a></td>
   <td class='td2' align='center'><input type='text' name='%toyid%' value='%quantity%' size='1' /> шт.</td>
   <td class='td2' align='right'>%price% руб.</td>
